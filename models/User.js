0bf874b20ac2c343a84fca6bbdb0dca676f4204a
chriscampaigne <campaignemc@gmail.com>
2022-09-25 17:44:28 -0400

    added passsword hashing

--- models/User.js
+++ models/User.js
@@ -45,8 +45,22 @@ User.init(
       }
     }
         },
-    {
-
+        {
+          hooks: {
+          // set up beforeCreate lifecycle "hook" functionality
+              async beforeCreate(newUserData) {
+              newUserData.password = await bcrypt.hash(newUserData.password, 10);
+              return newUserData;
+        }
+    },
+    hooks: {
+      // set up beforeCreate lifecycle "hook" functionality
+          async beforeUpdate(updatedUserData) {
+          updatedUserData.password = await bcrypt.hash(updatedUserData.password, 10);
+          return updatedUserData;
+    }
+},
+      
     // TABLE CONFIGURATION OPTIONS GO HERE (https://sequelize.org/v5/manual/models-definition.html#configuration))
     // pass in our imported sequelize connection (the direct connection to our database)
     sequelize,
@@ -60,5 +74,7 @@ User.init(
     modelName: 'user'
   }
 );
+const bcrypt = require('bcrypt');
+
 
 module.exports = User;
